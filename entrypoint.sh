d9d10a7dab606e3c3af6b5f7d5df7cbf8ff42260
srikanth-srinivas <srikanth.2001@outlook.com>
2024-05-23 17:20:33 +0530

    entry point script to run the pipeline in a docker image

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Check if Nextflow is installed
+if ! command -v nextflow &>/dev/null; then
+    echo "Nextflow could not be found"
+    exit
+fi
+
+# Run the Nextflow pipeline with the provided arguments
+nextflow run mapping.nf -profile docker "$@"
