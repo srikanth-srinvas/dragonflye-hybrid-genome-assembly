ab6628f9f698474d2c298476f0898353376db864
srikanth-srinivas <srikanth.2001@outlook.com>
2024-05-24 13:46:06 +0530

    updated entrypoint.sh to match nf-core framekwork

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
+set -e
 
-# Check if Nextflow is installed
-if ! command -v nextflow &>/dev/null; then
-    echo "Nextflow could not be found"
-    exit
-fi
-
-# Run the Nextflow pipeline with the provided arguments
-nextflow run main.nf -profile docker "$@"
+source activate bio_env
+nextflow run "$@"
